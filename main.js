442348193912e9dbf5b3c06142699d32d619e382
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-02 11:54:40 +0600

    update

---
+++ main.js
@@ -0,0 +1,33 @@
+// const: value will not change
+// let: value can change
+// var: we showed not use it
+
+// variable
+let amount = 92;
+
+//condition
+if (amount > 100) {
+} else {
+}
+
+// array
+const numbers = [1, 2342, 54, 32];
+console.log(numbers);
+
+// loop
+// for loop
+for (const num of numbers) {
+  console.log(num);
+}
+
+const fruits = ["orange", "apple", "banana"];
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+// while loop
+const number = 5;
+if (number < 10) {
+  console.log("choto ekta number paisi");
+}
+console.log("something new is cooing");
