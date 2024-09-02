f851ed4317eb4554ca21504672b0dff10ca187a5
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-02 15:15:39 +0600

    update

--- main.js
+++ main.js
@@ -31,3 +31,43 @@ if (number < 10) {
   console.log("choto ekta number paisi");
 }
 console.log("something new is cooing");
+
+// while loop
+let num = 0;
+while (num < 5) {
+  console.log("looping", num);
+  num++;
+}
+
+// range
+let rangeNum = 1;
+let sum = 0;
+while (rangeNum <= 10) {
+  console.log("Number:", rangeNum);
+  sum = sum + rangeNum;
+  rangeNum++;
+}
+console.log("Total: ", sum);
+
+sum = 0;
+rangeNum = 50;
+while (rangeNum <= 100) {
+  console.log("Number:", rangeNum);
+  sum = sum + rangeNum;
+  rangeNum++;
+}
+console.log("Total: ", sum);
+
+// even number
+let i = 1;
+while (i <= 10) {
+  if (i % 2 == 0) {
+    console.log("even number: ", i);
+  }
+  i++;
+}
+
+// for loop
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
